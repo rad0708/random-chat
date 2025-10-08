1c287bfca76e286d83393f330f14b485002ce913
정진원 <wrtyersd@gmail.com>
2025-10-08 22:32:02 +0900

    자동 업로드: 2025-10-08 22:32:02.89

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,22 @@
 
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Chat from './pages/Chat'
 import './styles/index.css'
 
-const router = createBrowserRouter([
-  { path: '/', element: <Home/> },
-  { path: '/chat', element: <Chat/> },
-])
+function Home(){
+  return <div className='flex flex-col items-center justify-center h-screen gap-4'>
+    <h1 className='text-4xl font-bold'>ShuffleChat</h1>
+    <Link to='/chat' className='px-4 py-2 bg-black text-white rounded'>채팅 시작</Link>
+  </div>
+}
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />)
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/chat' element={<Chat/>}/>
+    </Routes>
+  </BrowserRouter>
+)
