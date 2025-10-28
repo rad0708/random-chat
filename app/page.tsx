5f523b69a0f657909ac8d33672bbebfc7d0ac3d2
정진원 <wrtyersd@gmail.com>
2025-10-28 15:05:12 +0900

    로컬 강제 업로드: 2025-10-28 15:05:12.42

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ChatContainer } from "@/components/chat-container"
-
-export default function Home() {
-  return (
-    <main className="h-screen w-full">
-      <ChatContainer />
-    </main>
-  )
-}
